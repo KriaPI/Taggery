4df1f09a52dc4e19691664e6db1c6ae5931fe02c
KriaPia <kristoffer.berg8@gmail.com>
2026-06-15 20:45:05 +0200

    Implemented basic setup UI

--- lib/ui/pages/setup_page.dart
+++ lib/ui/pages/setup_page.dart
@@ -1,10 +1,21 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:taggery/ui/components/directory_path_selector.dart';
 
 class SetupPage extends StatelessWidget {
   const SetupPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Setup.")));
+    return Scaffold(body: 
+      Center(
+        child: DirectoryPathSelector(
+          buttonLabel: "Set root folder",
+          buttonCallback: () {
+            // TODO: implement logic to persist a folder path, and actually set it.
+            FilePicker.getDirectoryPath();
+          },
+        )
+    ));
   }
 }
