99a735ceeacebb3f1814b288131c80f1ea70b7b6
KriaPia <kristoffer.berg8@gmail.com>
2026-07-29 14:30:20 +0200

    Added panning/zooming to viewer

--- lib/ui/pages/home/image_viewer.dart
+++ lib/ui/pages/home/image_viewer.dart
@@ -3,10 +3,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:taggery/ui/components/buttons.dart';
 
+const ColorFilter grayscaleFilter = ColorFilter.matrix(<double>[
+  0.2126,
+  0.7152,
+  0.0722,
+  0,
+  0,
+  0.2126,
+  0.7152,
+  0.0722,
+  0,
+  0,
+  0.2126,
+  0.7152,
+  0.0722,
+  0,
+  0,
+  0,
+  0,
+  0,
+  1,
+  0,
+]);
+
 /// This widget allows the user to view an image.
-/// 
+///
 /// It additionally allows the user to show the previous and next image, view information about the image,
-/// and show it in black-and-white.
+/// and show it in black-and-white (monochrome).
 class ImageViewer extends ConsumerStatefulWidget {
   const ImageViewer({
     super.key,
@@ -29,7 +52,7 @@ class ImageViewer extends ConsumerStatefulWidget {
 }
 
 class ImageViewerState extends ConsumerState<ImageViewer> {
-  bool showInBlackAndWhite = false;
+  bool _isMonochrome = false;
 
   @override
   Widget build(BuildContext context) {
@@ -60,14 +83,29 @@ class ImageViewerState extends ConsumerState<ImageViewer> {
           child: Stack(
             alignment: .bottomCenter,
             children: [
-              // TODO: make it zoomable and pannable
-              ClipRRect(
-                borderRadius: BorderRadiusGeometry.all(.circular(8.0)),
-                clipBehavior: .antiAlias,
-                child: Image.file(widget.image),
+              // TODO: only change the mouse cursor when the image is zoomed into, otherwise use the default mouse cursor.
+              MouseRegion(
+                cursor: SystemMouseCursors.allScroll,
+                child: ClipRRect(
+                  borderRadius: .circular(8.0),
+                  child: InteractiveViewer(
+                    clipBehavior: Clip.antiAlias,
+                    minScale: 1.0,
+                    maxScale: 10.0,
+                    child: SizedBox.expand(
+                      child: _isMonochrome
+                          ? ColorFiltered(
+                              colorFilter: grayscaleFilter,
+                              child: Image.file(widget.image, fit: .contain),
+                            )
+                          : Image.file(widget.image, fit: .contain),
+                    ),
+                  ),
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
+                // TODO: figure out how to keep the state so that it is not unpinned when the widget is rebuilt. 
                 child: PinnableSlidingContainer(
                   children: [
                     SquareTonalIconButton(
@@ -81,8 +119,12 @@ class ImageViewerState extends ConsumerState<ImageViewer> {
                       onPressed: () {},
                     ),
                     SquareTonalIconButton(
-                      tooltip: "View in monochrome",
-                      onPressed: () {},
+                      tooltip: _isMonochrome
+                          ? "View in color"
+                          : "View in monochrome",
+                      onPressed: () {
+                        _isMonochrome = !_isMonochrome;
+                      },
                       icon: Icon(Icons.filter_b_and_w_rounded),
                     ),
                     SquareTonalIconButton(
@@ -102,11 +144,11 @@ class ImageViewerState extends ConsumerState<ImageViewer> {
 }
 
 /// A container arranging its children in a row that can be automatically hidden.
-/// 
+///
 /// This widget has a button that allows the container to be pinned (default) or unpinned.
-/// If the widget is unpinned, then it will slide down and out of view when the mouse is 
-/// not in its region. 
-/// 
+/// If the widget is unpinned, then it will slide down and out of view when the mouse is
+/// not in its region.
+///
 /// [children] are arranged to the sides of the pin button.
 class PinnableSlidingContainer extends StatefulWidget {
   const PinnableSlidingContainer({super.key, required this.children});
@@ -117,19 +159,19 @@ class PinnableSlidingContainer extends StatefulWidget {
 }
 
 class PinnableSlidingContainerState extends State<PinnableSlidingContainer> {
-  bool isPinned = true;
-  bool isShown = true;
+  bool _isPinned = true;
+  bool _isShown = true;
 
   @override
   Widget build(BuildContext context) {
     final hideButton = SquareTonalIconButton(
-      tooltip: isPinned ? "Unpin" : "Pin",
+      tooltip: _isPinned ? "Unpin" : "Pin",
       onPressed: () {
         setState(() {
-          isPinned = !isPinned;
+          _isPinned = !_isPinned;
         });
       },
-      icon: isPinned
+      icon: _isPinned
           ? Icon(Icons.arrow_drop_down_rounded)
           : Icon(Icons.arrow_drop_up_rounded),
     );
@@ -148,28 +190,28 @@ class PinnableSlidingContainerState extends State<PinnableSlidingContainer> {
       children: buttons,
     );
 
-    return isPinned
+    return _isPinned
         ? controls
         : MouseRegion(
             onEnter: (event) {
               setState(() {
-                isShown = true;
+                _isShown = true;
               });
             },
             onExit: (event) {
               setState(() {
-                isShown = false;
+                _isShown = false;
               });
             },
             child: ClipRect(
               child: AnimatedSlide(
-                offset: isShown ? Offset.zero : const Offset(0, 1.0),
+                offset: _isShown ? Offset.zero : const Offset(0, 1.0),
                 curve: Curves.easeInOut,
                 duration: Durations.short3,
                 child: AnimatedOpacity(
                   duration: Durations.short3,
                   curve: Curves.easeInOut,
-                  opacity: isShown ? 1.0 : 0.0,
+                  opacity: _isShown ? 1.0 : 0.0,
                   child: controls,
                 ),
               ),
