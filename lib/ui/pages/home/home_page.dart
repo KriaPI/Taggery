b5d8e87d15761c5c700b647bf5c4b7ed123f30dc
KriaPia <kristoffer.berg8@gmail.com>
2026-08-14 00:50:38 +0200

    Set up navigation with navigation rail
    The navigation rail can now be used to navigate between the gallery
    (home page) and the settings page.

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:taggery/ui/pages/home/content_area.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,7 +12,7 @@ class HomePage extends StatelessWidget {
         padding: const EdgeInsets.only(top: 16.0, right: 32.0),
         child: Row(
           children: [
-            const AppPageNavigator(),
+            const AppPageNavigator(currentIndex: 0),
             Expanded(
               child: const ContentArea(),
             ),
@@ -24,7 +25,27 @@ class HomePage extends StatelessWidget {
 
 /// Navigation rail
 class AppPageNavigator extends StatelessWidget {
-  const AppPageNavigator({super.key});
+  const AppPageNavigator({
+    super.key, 
+    required this.currentIndex,
+  });
+
+  final int currentIndex;
+
+  void _onItemTapped(int index, BuildContext context) {
+    // Map each index to your go_router paths
+    switch (index) {
+      case 0:
+        context.go("/"); // Replace with your actual route name
+        break;
+      case 1:
+        throw UnimplementedError();
+        //break;
+      case 2:
+        context.go('/settings');
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +57,8 @@ class AppPageNavigator extends StatelessWidget {
         child: Icon(Icons.edit_rounded),
       ),
       labelType: .all,
-      selectedIndex: 0,
+      selectedIndex: currentIndex,
+      onDestinationSelected: (value) => _onItemTapped(value, context),
       destinations: const [
         NavigationRailDestination(
           icon: Icon(Icons.photo_library_rounded),
